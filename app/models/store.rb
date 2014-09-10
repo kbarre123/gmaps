c56a72ad05bf7da21f66c41af02107c113bdb80f
KC Barrett <kbarre123@gmail.com>
2014-09-10 16:03:36 -0500

    Initial commit

---
+++ app/models/store.rb
@@ -0,0 +1,6 @@
+class Store < ActiveRecord::Base
+
+    geocoded_by :address
+    after_validation :geocode
+
+end
---
+++ db/migrate/20140910202901_create_stores.rb
@@ -0,0 +1,13 @@
+class CreateStores < ActiveRecord::Migration
+  def change
+    create_table :stores do |t|
+      t.float :latitude
+      t.float :longitude
+      t.string :address
+      t.text :description
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
